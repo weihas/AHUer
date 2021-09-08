41c8933eb502997d493cdf2199fac51dc8b4416d
Weiha Wang <weihas@icloud.com>
2021-09-09 00:14:19 +0800

    [init commit]

---
+++ AHUer/TimeTablePage/TimeTable.swift
@@ -0,0 +1,22 @@
+//
+//  TimeTable.swift
+//  AHUer
+//
+//  Created by WeIHa'S on 2021/9/8.
+//
+
+import Foundation
+
+struct TimeTable {
+    var a = 0
+}
+
+enum weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
