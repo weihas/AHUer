d4c6d6a0dfda280f34169de68444af5bbed9dae6
Weiha Wang <weihas@icloud.com>
2021-09-08 21:08:49 +0800

    Initial Commit

---
+++ AHUer/AHUerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AHUerApp.swift
+//  AHUer
+//
+//  Created by WeIHa'S on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct AHUerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AHUer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AHUer
+//
+//  Created by WeIHa'S on 2021/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
