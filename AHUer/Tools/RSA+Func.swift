c8c36a23302dc1ce89dc9c995b87285d8773cfa9
Weiha Wang <weihas@icloud.com>
2021-10-05 17:42:56 +0800

    [feat]: contained by cocoapods

---
+++ AHUer/Tools/RSA+Func.swift
@@ -0,0 +1,28 @@
+//
+//  RSA+Func.swift
+//  AHUer
+//
+//  Created by WeIHa'S on 2021/10/5.
+//
+
+import Foundation
+import SwiftyRSA
+
+extension String{
+    func rsaCrypto() -> String? {
+        let key = """
+-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBAI9fwyD3Hb5ryCjs7tR1n+SNkZgmUXxdMWl6g9L4s9xLsjsE0yjvaPeZRBIroo6Yc+laattX0WhTonMtAI/hP6/4G/ImBSsYUBilHL9BRQjXo7pfh02G93SycNxsOtLtatHJkSmDWjzj76RJypjpaBaMOIwMlU87pvaeX5d/gXorAgMBAAE=
+-----END RSA PUBLIC KEY-----
+"""
+        do {
+            let publicKey = try PublicKey(pemEncoded: key)
+            let clear = try ClearMessage(string: "123", using: .utf8)
+            let encrypted = try clear.encrypted(with: publicKey, padding: .PKCS1)
+            return encrypted.base64String
+        }catch{
+            print("加密失败")
+            return nil
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.0'
+
+target 'AHUer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AHUer
+  pod 'Moya', '~> 15.0'
+  pod 'SwiftyRSA'
+
+  target 'AHUerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AHUerUITests' do
+    # Pods for testing
+  end
+
+end
