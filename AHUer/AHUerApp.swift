590a8532e87dae8ed425dd2c78cab45d318a0a08
Weiha Wang <weihas@icloud.com>
2022-02-25 11:28:41 +0800

    [1.0.0] fix main file lose

---
+++ AHUer/AHUerApp.swift
@@ -0,0 +1,21 @@
+//
+//  AHUerApp.swift
+//  AHUer
+//
+//  Created by WeIHa'S on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct AHUerApp: App {
+    let persistenceController = PersistenceController.shared
+    let info = AHUAppInfo()
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(info)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
